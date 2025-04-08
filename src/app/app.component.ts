fb4dc2861de2ecf6d78660823115704c9cd55509
alwat83 <albertlwatson@gmail.com>
2025-04-08 21:50:11 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  imports: [RouterModule],
+  standalone: true,
+})
+export class AppComponent {
+  currentYear: number = new Date().getFullYear();
+  title = 'doseninja';
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
